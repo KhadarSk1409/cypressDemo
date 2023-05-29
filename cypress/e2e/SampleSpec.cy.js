58d45f53332e00cde7edf5c34b7eaea3c2326696
Khadar <shaikh@visualorbit.com>
2023-05-29 20:30:08 +0530

    Changes made

--- cypress/e2e/SampleSpec.cy.js
+++ cypress/e2e/SampleSpec.cy.js
@@ -5,13 +5,18 @@ describe('template spec', function() {
   it('sample passes', function () {
     cy.visit('https://example.cypress.io')
     cy.wait(6000)
-    cy.get('#navbar > ul:nth-child(1) > li:nth-child(3) > a')
-    cy.get('h2').should('have.text', 'CommandsUtilitiesCypress API');
-    
+    cy.get('#navbar > ul:nth-child(1) > li:nth-child(3) > a');
   })
 
-  it('sample fails', () =>{
+  it('sample assertion', () =>{
     cy.visit('https://example.cypress.io')
-    cy.get('h1').should('have.text', 'Xyz');
+    cy.get('h2').should('have.text', 'CommandsUtilitiesCypress API');
   })
+
+  it('sample click', function () {
+    cy.visit('https://example.cypress.io')
+    cy.get('#navbar > ul:nth-child(1) > li:nth-child(2) > a').click()
+    cy.get('h1').should('have.text','Utilities');
+  })
+
 })
